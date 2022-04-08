e2ce027070f32149d83e0016592e7240179d7550
Emanuel-Ramos <manuel_live_ac@hotmail.com>
2022-04-08 09:53:48 -0300

    feat: aplicando componente container no footer

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -9,132 +9,144 @@ import Link from 'src/components/ui/Link'
 import SROnly from 'src/components/ui/SROnly'
 import { mark } from 'src/sdk/tests/mark'
 
+import Container from '../Container'
+import Section from '../Section'
 import FooterLinks from './FooterLinks'
 
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer__section">
-        <section className="footer__logo">
-          <UIIcon
-            component={
-              <img src="/Beauty.svg" alt="Beauty logo" loading="lazy" />
-            }
-          />
-          <p className="footer__description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor.
-          </p>
-        </section>
-        <FooterLinks />
-        <section className="footer__social">
-          <p className="title-sub-subsection">Follow us</p>
-          <UIList variant="unordered">
-            <li>
-              <Link
-                as="a"
-                href="https://www.facebook.com/"
-                title="Facebook"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <UIIcon
-                  component={<Icon width={24} height={24} name="Facebook" />}
-                />
-              </Link>
-            </li>
-            <li>
-              <Link
-                as="a"
-                href="https://www.instagram.com/"
-                title="Instagram"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <UIIcon
-                  component={<Icon width={24} height={24} name="Instagram" />}
-                />
-              </Link>
-            </li>
-            <li>
-              <Link
-                as="a"
-                href="https://www.pinterest.com/"
-                title="Pinterest"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <UIIcon
-                  component={<Icon width={24} height={24} name="Pinterest" />}
-                />
-              </Link>
-            </li>
-            <li>
-              <Link
-                as="a"
-                href="https://twitter.com/"
-                title="Twitter"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <UIIcon
-                  component={<Icon width={24} height={24} name="Twitter" />}
-                />
-              </Link>
-            </li>
-          </UIList>
-        </section>
-      </div>
+      <Section className="footer">
+        <Container>
+          <div className="footer__section">
+            <section className="footer__logo">
+              <UIIcon
+                component={
+                  <img src="/Beauty.svg" alt="Beauty logo" loading="lazy" />
+                }
+              />
+              <p className="footer__description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor.
+              </p>
+            </section>
+            <FooterLinks />
+            <section className="footer__social">
+              <p className="title-sub-subsection">Follow us</p>
+              <UIList variant="unordered">
+                <li>
+                  <Link
+                    as="a"
+                    href="https://www.facebook.com/"
+                    title="Facebook"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <UIIcon
+                      component={
+                        <Icon width={24} height={24} name="Facebook" />
+                      }
+                    />
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    as="a"
+                    href="https://www.instagram.com/"
+                    title="Instagram"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <UIIcon
+                      component={
+                        <Icon width={24} height={24} name="Instagram" />
+                      }
+                    />
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    as="a"
+                    href="https://www.pinterest.com/"
+                    title="Pinterest"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <UIIcon
+                      component={
+                        <Icon width={24} height={24} name="Pinterest" />
+                      }
+                    />
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    as="a"
+                    href="https://twitter.com/"
+                    title="Twitter"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <UIIcon
+                      component={<Icon width={24} height={24} name="Twitter" />}
+                    />
+                  </Link>
+                </li>
+              </UIList>
+            </section>
+          </div>
 
-      <div className="footer__note">
-        <UIPaymentMethods>
-          <p className="title-sub-subsection">Payment Methods</p>
-          <UIList>
-            <li>
-              <Icon width={34} height={24} name="Visa" />
-              <SROnly text="Visa" />
-            </li>
-            <li>
-              <Icon width={34} height={24} name="Diners" />
-              <SROnly text="Diners Club" />
-            </li>
-            <li>
-              <Icon width={34} height={24} name="Mastercard" />
-              <SROnly text="Mastercard" />
-            </li>
-            <li>
-              <Icon width={34} height={24} name="EloCard" />
-              <SROnly text="Elo Card" />
-            </li>
-            <li>
-              <Icon width={34} height={24} name="PayPal" />
-              <SROnly text="PayPal" />
-            </li>
-            <li>
-              <Icon width={34} height={24} name="Stripe" />
-              <SROnly text="Stripe" />
-            </li>
-            <li>
-              <Icon width={34} height={24} name="GooglePay" />
-              <SROnly text="Google Pay" />
-            </li>
-            <li>
-              <Icon width={34} height={24} name="ApplePay" />
-              <SROnly text="Apple Pay" />
-            </li>
-          </UIList>
-        </UIPaymentMethods>
+          <div className="footer__note">
+            <UIPaymentMethods>
+              <p className="title-sub-subsection">Payment Methods</p>
+              <UIList>
+                <li>
+                  <Icon width={34} height={24} name="Visa" />
+                  <SROnly text="Visa" />
+                </li>
+                <li>
+                  <Icon width={34} height={24} name="Diners" />
+                  <SROnly text="Diners Club" />
+                </li>
+                <li>
+                  <Icon width={34} height={24} name="Mastercard" />
+                  <SROnly text="Mastercard" />
+                </li>
+                <li>
+                  <Icon width={34} height={24} name="EloCard" />
+                  <SROnly text="Elo Card" />
+                </li>
+                <li>
+                  <Icon width={34} height={24} name="PayPal" />
+                  <SROnly text="PayPal" />
+                </li>
+                <li>
+                  <Icon width={34} height={24} name="Stripe" />
+                  <SROnly text="Stripe" />
+                </li>
+                <li>
+                  <Icon width={34} height={24} name="GooglePay" />
+                  <SROnly text="Google Pay" />
+                </li>
+                <li>
+                  <Icon width={34} height={24} name="ApplePay" />
+                  <SROnly text="Apple Pay" />
+                </li>
+              </UIList>
+            </UIPaymentMethods>
 
-        <div className="footer__copyright / text-body-small">
-          <p>This website uses VTEX technology</p>
-          <p>
-            In-store price may vary. Prices and offers are subject to change.
-            2021 Store name. All rights reserved. Store is a trademark of Store
-            and its affiliated companies.
-          </p>
-          <address>Mount St, 000, New York / NY - 00000.</address>
-        </div>
-      </div>
+            <div className="footer__copyright / text-body-small">
+              <p>This website uses VTEX technology</p>
+              <p>
+                In-store price may vary. Prices and offers are subject to
+                change. 2021 Store name. All rights reserved. Store is a
+                trademark of Store and its affiliated companies.
+              </p>
+              <address>Mount St, 000, New York / NY - 00000.</address>
+            </div>
+          </div>
+        </Container>
+      </Section>
     </footer>
   )
 }
