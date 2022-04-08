0e30ac247ed635afc2144ee50d25db38ead04151
Daniel Schuab <danielschuabramos@gmail.com>
2022-04-08 16:30:16 -0300

    feat(home): adiciona props ao componente do mosaico

--- src/components/sections/Mosaic/Mosaic.tsx
+++ src/components/sections/Mosaic/Mosaic.tsx
@@ -3,98 +3,63 @@ import Section from 'src/components/common/Section'
 import Link from 'src/components/ui/Link'
 import { useWidescreen } from 'src/sdk/ui/useWidescreen'
 
-const Mosaic = () => {
+interface MosaicProps {
+  topic: string
+  title: string
+  buttonLink: string
+  buttonLabel: string
+  desktopImages: string[]
+  mobileImages: string[]
+}
+
+const Mosaic = (props: MosaicProps) => {
+  const { topic, title, buttonLink, buttonLabel, desktopImages, mobileImages } =
+    props
+
   const { isWidescreen } = useWidescreen()
 
   return (
     <Section className="mosaic">
       <div className="mosaic__grid">
         <div className="mosaic-call">
-          <h3 className="mosaic-call__topic">Scanner Facial</h3>
-
-          <h2 className="mosaic-call__title">
-            Descubra a sua rotina skincare ideal com o scanner facial beauty
-          </h2>
-
-          <Link to="/scanner">Faça o seu agora</Link>
+          <h3 className="mosaic-call__topic">{topic}</h3>
+          <h2 className="mosaic-call__title">{title}</h2>
+          <Link to={buttonLink}>{buttonLabel}</Link>
         </div>
 
         {isWidescreen !== null ? (
           isWidescreen ? (
             <>
-              <div className="mosaic__image-wrapper">
-                <img
-                  className="mosaic__image"
-                  src="/mosaico-banner-01.jpg"
-                  alt=""
-                />
-              </div>
-
-              <div className="mosaic__image-wrapper">
-                <img
-                  className="mosaic__image"
-                  src="/mosaico-banner-02.jpg"
-                  alt=""
-                />
-              </div>
-
-              <div className="mosaic__image-wrapper">
-                <img
-                  className="mosaic__image"
-                  src="/mosaico-banner-03.jpg"
-                  alt=""
-                />
-              </div>
-
-              <div className="mosaic__image-wrapper">
-                <img
-                  className="mosaic__image"
-                  src="/mosaico-banner-04.jpg"
-                  alt=""
-                />
-              </div>
-
-              <div className="mosaic__image-wrapper">
-                <img
-                  className="mosaic__image"
-                  src="/mosaico-banner-05.jpg"
-                  alt=""
-                />
-              </div>
+              {desktopImages.length > 0 &&
+                desktopImages.map((desktopImage, index) => {
+                  return (
+                    <div key={index} className="mosaic__image-wrapper">
+                      <img
+                        className="mosaic__image"
+                        src={desktopImage}
+                        alt={`${index}ª imagem do mosaico`}
+                      />
+                    </div>
+                  )
+                })}
             </>
           ) : (
             <div className="mosaic__images">
-              <div className="mosaic__image-wrapper mosaic__image-wrapper--1">
-                <img
-                  className="mosaic__image"
-                  src="/mosaico-banner-01.jpg"
-                  alt=""
-                />
-              </div>
-
-              <div className="mosaic__image-wrapper mosaic__image-wrapper--2">
-                <img
-                  className="mosaic__image"
-                  src="/mosaico-banner-02.jpg"
-                  alt=""
-                />
-              </div>
-
-              <div className="mosaic__image-wrapper mosaic__image-wrapper--3">
-                <img
-                  className="mosaic__image"
-                  src="/mosaico-banner-03.jpg"
-                  alt=""
-                />
-              </div>
-
-              <div className="mosaic__image-wrapper mosaic__image-wrapper--4">
-                <img
-                  className="mosaic__image"
-                  src="/mosaico-banner-05.jpg"
-                  alt=""
-                />
-              </div>
+              {mobileImages.length > 0 &&
+                mobileImages.map((mobileImage, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className={`mosaic__image-wrapper mosaic__image-wrapper--${index}`}
+                    >
+                      <img
+                        className="mosaic__image"
+                        src={mobileImage}
+                        alt={`${index}ª imagem do mosaico`}
+                      />
+                    </div>
+                  )
+                })}
             </div>
           )
         ) : (
