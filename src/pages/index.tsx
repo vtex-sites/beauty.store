c614754584ac9c215134a72e3386e6e167c0089f
Gabriel Buquer <gabriel.buquer@m3ecommerce.com>
2022-04-13 10:28:10 -0300

    feat: remove swiper do banner principal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -78,36 +78,20 @@ function Page(props: Props) {
       />
 
       <HomeBanner
-        banners={[
-          {
-            imageBanner:
-              'https://beauty.vteximg.com.br/arquivos/home-banner-01.jpg',
-            imageBannerMobile:
-              'https://beauty.vteximg.com.br/arquivos/home-banner-mobile-01.jpg',
-            title: 'Cuide da sua pele com produtos feitos para você!',
-            subtitle:
-              'Através do nosso scanner, você consegue identificar o seu perfil de beleza',
-            link: '/scanner',
-            quote:
-              'Quando comecei a usar os produtos indicados pela beauty minha pele e cabelo mudaram totalmente! Recomendo para as minhas amigas sempre!',
-            seeMoreTitle: 'Serum treatment',
-            seeMoreLink: '/serum-treatment',
-          },
-          {
-            imageBanner:
-              'https://beauty.vteximg.com.br/arquivos/home-banner-01.jpg',
-            imageBannerMobile:
-              'https://beauty.vteximg.com.br/arquivos/home-banner-mobile-01.jpg',
-            title: 'Cuide da sua pele com produtos feitos para você!',
-            subtitle:
-              'Através do nosso scanner, você consegue identificar o seu perfil de beleza',
-            link: '/scanner',
-            quote:
-              'Quando comecei a usar os produtos indicados pela beauty minha pele e cabelo mudaram totalmente! Recomendo para as minhas amigas sempre!',
-            seeMoreTitle: 'Serum treatment',
-            seeMoreLink: '/serum-treatment',
-          },
-        ]}
+        banner={{
+          imageBanner:
+            'https://beauty.vteximg.com.br/arquivos/home-banner-01.jpg',
+          imageBannerMobile:
+            'https://beauty.vteximg.com.br/arquivos/home-banner-mobile-01.jpg',
+          title: 'Cuide da sua pele com produtos feitos para você!',
+          subtitle:
+            'Através do nosso scanner, você consegue identificar o seu perfil de beleza',
+          link: '/scanner',
+          quote:
+            'Quando comecei a usar os produtos indicados pela beauty minha pele e cabelo mudaram totalmente! Recomendo para as minhas amigas sempre!',
+          seeMoreTitle: 'Serum treatment',
+          seeMoreLink: '/serum-treatment',
+        }}
       />
       <Incentives
         incentives={[
